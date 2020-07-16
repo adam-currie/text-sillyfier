035fcb78e5f6cb87dafb5620ede18ccb230c1e04
adam-currie <tfxflrstx@gmail.com>
2020-07-16 00:35:23 -0400

    added source

---
+++ 1 line silly (2 silly 4 me).py
@@ -0,0 +1,1 @@
+﻿while (True): print(''.join([(x.upper() if i%2==0 else x.lower()) for i, x in enumerate(input())]))
---
+++ silly.py
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/env python
+
+
+try:
+    import pyperclip
+    we_pyperclipin = True;
+except ModuleNotFoundError:
+    we_pyperclipin = False;
+    print("Install pyperclip if you want silly text to be automatically copied to your clipboard (pip install pyperclip)")
+
+while (True):
+    silly = ''.join([(x.upper() if i%2==0 else x.lower()) for i, x in enumerate(input())])
+    if we_pyperclipin: pyperclip.copy(silly)
+    print(silly)
